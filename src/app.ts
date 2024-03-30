40fe55e9113273fda47f26d5fa4ae83581d7355b
samychowdhury01 <samychowdhury84@gmail.com>
2024-03-30 10:46:32 +0600

    added global error handler and not found handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from  'express';
 import cors from 'cors'
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 const app : Application= express()
 // parsers
 app.use(express.json());
@@ -8,5 +10,11 @@ app.use(cors());
 app.get('/', (req : Request, res : Response) => {
   res.send('Hello Tab startup!')
 })
+
+// global error handler
+app.use(globalErrorHandler);
+
+// not found Route
+app.use(notFound)
 
 export default app
